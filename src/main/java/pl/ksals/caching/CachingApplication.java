b768df8a0291821ff4ec0388d7f1bcb28dddb33e
lukasz <slaskww@gmail.com>
2020-03-27 18:13:30 +0100

    Caching-example initialization

---
+++ src/main/java/pl/ksals/caching/CachingApplication.java
@@ -0,0 +1,13 @@
+package pl.ksals.caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingApplication.class, args);
+	}
+
+}
