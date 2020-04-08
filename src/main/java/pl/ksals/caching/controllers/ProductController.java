70c219bd95dba1351e4f7c5c268bf166b64312e8
lukasz <slaskww@gmail.com>
2020-04-08 23:46:16 +0200

    Add crud methods to ProductController

--- src/main/java/pl/ksals/caching/controllers/ProductController.java
+++ src/main/java/pl/ksals/caching/controllers/ProductController.java
@@ -1,7 +1,9 @@
 package pl.ksals.caching.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import pl.ksals.caching.domain.Product;
+import pl.ksals.caching.model.Products;
 import pl.ksals.caching.services.ProductCrudService;
 
 @RestController
@@ -12,6 +14,34 @@ public class ProductController {
 
     public ProductController(ProductCrudService productCrudService) {
         this.productCrudService = productCrudService;
+    }
+
+    @GetMapping
+    public Products getAll(){
+        return new Products(productCrudService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable final Long id){
+        return productCrudService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody final Product product){
+        return productCrudService.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateProduct(@RequestBody final Product product, @PathVariable final Long id){
+        productCrudService.updateProduct(product, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable final Long id){
+        productCrudService.deleteProduct(id);
     }
 
 }
