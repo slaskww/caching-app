71afe5aec982a3ee0abe9b9f72a3a2edee41b43a
lukasz <slaskww@gmail.com>
2020-04-21 15:53:53 +0200

    Add cache evicting mechanism to ProductService

--- src/main/java/pl/ksals/caching/services/ProductCrudService.java
+++ src/main/java/pl/ksals/caching/services/ProductCrudService.java
@@ -1,7 +1,9 @@
 package pl.ksals.caching.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import pl.ksals.caching.domain.Product;
 import pl.ksals.caching.repository.ProductRepository;
@@ -18,24 +20,38 @@ public class ProductCrudService {
         this.productRepository = productRepository;
     }
 
+    //adnotacja sprawia, że wynik metody trafia do cache
     @Cacheable(cacheNames = "products")
     public List<Product> getAll(){
         log.info("Get all products");
        return productRepository.findAll();
     }
 
-    @Cacheable(cacheNames = "products")
+    //adnotacja sprawia, że wynik metody trafia do cache
+    @Cacheable(cacheNames = "single-product")
     public Product getById(final Long id){
         log.info("Get product by id " + id);
         return productRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Product with id doesn't exist"));
     }
 
+    //adnotacją CacheEvict sprawia, że uruchonienie metody powoduje usunięcie danych z cache,
+    //adnotacja Caching grupuje adnotacje  @CacheEvict, a także  @Cacheable
+    @Caching(evict = {
+            @CacheEvict(cacheNames = "products", allEntries = true),
+            @CacheEvict(cacheNames = "single-product", allEntries = true)
+    })
     public Product createProduct(final Product product){
         log.info("Create product");
         product.setId(null);
         return productRepository.save(product);
     }
 
+    //adnotacją CacheEvict sprawia, że uruchonienie metody powoduje usunięcie danych z cache,
+    //adnotacja Caching grupuje adnotacje  @CacheEvict, a także  @Cacheable
+    @Caching(evict = {
+            @CacheEvict(cacheNames = "products", allEntries = true),
+            @CacheEvict(cacheNames = "single-product", allEntries = true)
+    })
     public Product updateProduct(final Product product, final Long id){
         log.info("Update product");
         final Product existingProduct = getById(id);
@@ -45,6 +61,12 @@ public class ProductCrudService {
         return productRepository.save(existingProduct);
     }
 
+    //adnotacją CacheEvict sprawia, że uruchonienie metody powoduje usunięcie danych z cache,
+    //adnotacja Caching grupuje adnotacje  @CacheEvict, a także  @Cacheable
+    @Caching(evict = {
+            @CacheEvict(cacheNames = "products", allEntries = true),
+            @CacheEvict(cacheNames = "single-product", allEntries = true)
+    })
     public void deleteProduct(final Long id){
         log.info("Delete product " + id);
         productRepository.deleteById(id);
